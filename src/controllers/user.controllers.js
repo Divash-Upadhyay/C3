27033ebc2e11721acf827460a3b2ab1fcd213741
Divash-Upadhyay <76831671+Divash-Upadhyay@users.noreply.github.com>
2022-03-21 10:56:54 +0530

    4th commit

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -20,82 +20,89 @@ const router = express.Router();
 router.post(
     "",
     body("id")
-    .isNumeric()
-    .withMessage("invalid id")
-    .bail()
-    .custom(async(value) =>{
-      const user= await User.findOne({id:value});
-      if(user){
-        throw new Error("incorrect id");
-      }
-      return true;
-    }),
+        .isNumeric()
+        .withMessage("invalid id")
+        .bail()
+        .custom(async (value) => {
+            const user = await User.findOne({ id: value });
+            if (user) {
+                throw new Error("incorrect id");
+            }
+            return true;
+        }),
     body("first_name")
-    .isString()
-    .isLength({min:3 , max:30}),
+        .isString()
+        .isLength({ min: 3, max: 30 }),
     body("last_name")
-    .isString()
-    .isLength({min:3 , max:30}),
+        .isString()
+        .isLength({ min: 3, max: 30 }),
     body("age")
-    .isAlphanumeric()
-    .custom((value) =>{
-        if (value>1||value<150){
-    throw new Error("age must be between 1 and 150")
-}
-return true
-    }),
+        .isAlphanumeric()
+        .custom((value) => {
+            if (value > 1 || value < 150) {
+                throw new Error("age must be between 1 and 150")
+            }
+            return true
+        }),
     body("email")
-    .isString(),
+        .isString(),
     body("profile_pic")
-    .isAlphanumeric(),
+        .isAlphanumeric(),
 
     async (req, res) => {
-    try {
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        let newErrors;
-        newErrors = errors.array().map((err) => {
-          return { key: err.param, message: err.msg };
-        });
-        return res.status(400).send({ errors: newErrors });
-      }
-      const user = await User.create(req.body);
-      return res.send(user);
-    } catch (err) {
-      return res.status(500).send({ message: err.message });
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                let newErrors;
+                newErrors = errors.array().map((err) => {
+                    return { key: err.param, message: err.msg };
+                });
+                return res.status(400).send({ errors: newErrors });
+            }
+            const user = await User.create(req.body);
+            return res.send(user);
+        } catch (err) {
+            return res.status(500).send({ message: err.message });
+        }
     }
-  }
 );
 
 
 router.post("/dp", uploaddp("profile_pic"), async (req, res) => {
     try {
-      const user = await User.create({
-        id: req.body.id,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        age: req.body.age,
-        email: req.body.email,
-        password: req.body.password,
-        
-        profile_pic: req.file.path,
-      });
-  
-      return res.send(user);
-    } catch (err) {
-      return res.status(500).send({ message: err.message });
-    }
-  });
+        const user = await User.create({
+            id: req.body.id,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            age: req.body.age,
+            email: req.body.email,
+            password: req.body.password,
 
-  
-router.get("",async(req,res)=>{
-    try{
-        const users=await User.find().lean().exec();
-        return res.send(users);
+            profile_pic: req.file.path,
+        });
+
+        return res.send(user);
+    } catch (err) {
+        return res.status(500).send({ message: err.message });
     }
-    catch(e){
+});
+
+
+router.get("", async (req, res) => {
+    try {
+        const page = req.query.page || 1;
+        const pagesize = req.query.pagesize || 10;
+
+        const users = await User.find().skip().limit(pagesize).lean().exec();
+
+        const totalPages = Math.ceil(
+            (await Product.find().countDocuments()) / pagesize
+        );
+        return res.send({users, totalpages});
+    }
+    catch (e) {
         res.status(500).send(e.message);
     }
 })
 
-module.exports=router;
+module.exports = router;
